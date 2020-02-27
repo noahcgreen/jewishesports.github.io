b2a11d89395057fdacc90785edbce61944d87752
Noah C. Green <noahc.green@icloud.com>
2020-02-27 14:33:48 -0500

    First steps in Jekyll

--- scripts/nhei.js
+++
@@ -1,6 +0,0 @@
-function switchTheme() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
-}
-const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
-window.addEventListener('change', switchTheme, false)''
